6a6a049dadb0eee61209ac1992c84de65f9a9204
Nithin <vooturinithinkumar@gmail.com>
2026-05-20 16:47:52 +0530

    configure httpStatusConstants & httpResponses

---
+++ src/constants/httpResponses.js
@@ -0,0 +1,16 @@
+
+const httpMessages = {
+  
+    USER_EMAIL_DUPLICATE: "OOPS!!, email already exists try with different One",
+    USER_INPDATA_VALIDATED: "Data validation Success",
+    USER_REGISTRATION_SUCCESS: "user regestration is successfull!",
+    USER_REGISTRATION_FAIL: "registration failed",
+    INVALID_PASSWORD: "You have entered incorrect password, Please enter correct one",
+    OPS_SUCCESS_STATUS: "success",
+    OPS_FAIL_STATUS: "fail",
+    ACCOUNT_NOT_FOUND: "Account does not exists with the provided email, Please Register Now",
+    INCORRECT_PASSWORD: "You have entered wrong password, Please provide valid password",
+    ERROR: "error"
+}
+
+export default httpMessages;
