5d4a3d670774a5266193f67a8d459bd05b0162d2
Nithin <vooturinithinkumar@gmail.com>
2026-05-29 11:37:54 +0530

    define user-end responses and configure global-error middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import authRoutes from "./src/routes/authRoutes.js";
+import {globalErrorMiddleware} from "./src/middleware/errorMiddleware.js";
 const app = express();
 
 
@@ -14,8 +15,9 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan(':time  :method for  :url was received - StatusCode :status - :response-time ms'));
+app.use(globalErrorMiddleware);
 app.use("/auth",authRoutes);
-app.get('/health', () => {
+app.get('/health', (req, res) => {
     res.status(200).json({
         "status": "success",
         "message": "ShopCart API is running 🆙"
